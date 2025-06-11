8c4d35b78c8c2d93fdab2f90bde43269ccfdff75
Bielis08 <bielmonteroroman08@gmail.com>
2025-06-11 17:33:15 +0200

    promocion cambiada

--- src/lib/promociones.ts
+++ src/lib/promociones.ts
@@ -3,7 +3,7 @@ const promociones = [
         titulo: "2x1 en Cafés",
         descripcion: "Llevate dos cafés por el precio de uno. ¡Disfruta de nuestro cafés con un amigo!",
         imagen: "cafe.jpg",
-        precio: 3,
+        precio: 4,
         descuento: 50,
     }
 ]
